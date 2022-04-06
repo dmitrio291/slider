15503b0700befed22204ec9c8397708702f7f0d0
Dmitry <dikck.dmitry@yandex.ru>
2022-04-06 13:31:57 +0400

    update script.js

--- js/script.js
+++ js/script.js
@@ -1,8 +1,10 @@
 document.addEventListener('DOMContentLoaded', () => {
+  // Переменные для работы слайдера
   const slider = document.querySelector('.slider');
   const demoBtn = document.querySelector('.demo-btn');
   const sliderPreview = document.querySelectorAll('.slider__thumbs-item');
 
+  // Ф-ия коллбек для добавления или удаления активного класса у превью
   const sliderPreviewCallback = () => {
     const index = swiper.activeIndex;
 
@@ -13,12 +15,14 @@ document.addEventListener('DOMContentLoaded', () => {
     sliderPreview[index].classList.add('active');;
   };
 
+  // При клике на превью переключает слайд, который соотвествует его индексу
   sliderPreview.forEach((preview, index) => {
     preview.addEventListener('click', () => {
       swiper.slideTo(index);
     });
   });
 
+  // При клике на кнопку Начать демонстрацию показывает слайдер и прячет эту кнопку
   demoBtn.addEventListener('click', () => {
     setTimeout(() => {
       slider.classList.remove('slider--show');
@@ -26,6 +30,7 @@ document.addEventListener('DOMContentLoaded', () => {
     }, 5000);
   });
 
+  // Инициализация слайдера
   const swiper = new Swiper(slider, {
     pagination: {
       el: '.slider__pagination',
@@ -42,5 +47,6 @@ document.addEventListener('DOMContentLoaded', () => {
     },
   });
 
+  // Вызов ф-ии sliderPreviewCallback() для добавления класса active у превью по умолчанию
   sliderPreviewCallback();
 });
